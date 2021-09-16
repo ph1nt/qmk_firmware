4c69afd50321d8c71643dfa56383bdcbd82e5bdf
H1nt <hp0@me.com>
2021-09-16 18:45:38 +0200

    space alt mod tap added

--- keyboards/rp2040_HNA_planck/keymaps/default/keymap.c
+++ keyboards/rp2040_HNA_planck/keymaps/default/keymap.c
@@ -19,13 +19,14 @@ enum layer_names {
 #define LCBRACKET LSFT(KC_LBRACKET)
 #define RCBRACKET LSFT(KC_RBRACKET)
 #define RETURN LSFT(KC_ENTER)
+#define ALT_SPACE MT(MOD_RALT, KC_SPACE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPACE,
     KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_ENTER,
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_QUOTE,
-    FNC,            KC_LCTRL,       KC_LALT,        KC_LGUI,        LOWER,          KC_LSHIFT,      KC_SPACE,            RAISE,          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT
+    FNC,            KC_LCTRL,       KC_LALT,        KC_LGUI,        LOWER,          KC_LSHIFT,      ALT_SPACE,      RAISE,          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT
   ),
   [_LOWER] = LAYOUT(
     KC_GRAVE,       KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RPRN,        KC_TRANSPARENT,
